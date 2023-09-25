dacebe078a1f37d127c94db5d78b6c9f43e31052
richard_futo <40018431+Safran3k@users.noreply.github.com>
2023-09-25 08:55:02 +0200

    Update demo.hs

--- demo.hs
+++ demo.hs
@@ -2,14 +2,14 @@ module Masodik_hazi where
 
 isTriangle :: (Num a, Ord a) => a -> a -> a -> Bool
 isTriangle a b c
-    | a <= 0 || b <= 0 || c <= 0 = False  
+    | a < 0 || b < 0 || c < 0 = False  
     | a + b <= c || a + c <= b || b + c <= a = False  
     | otherwise = True 
 
 
 isPythagoreanTriple :: Int -> Int -> Int -> Bool
 isPythagoreanTriple a b c
-    | a <= 0 || b <= 0 || c <= 0 = False  
+    | a < 0 || b < 0 || c < 0 = False  
     | a^2 + b^2 == c^2 || a^2 + c^2 == b^2 || b^2 + c^2 == a^2 = True
     | otherwise = False  
 
